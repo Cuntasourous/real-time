58e2cd1195a57076c304f2e9f9ed565e5224f6a0
araed <ameenaraed221@gmail.com>
2024-07-27 18:20:08 +0300

    docker

--- docker.sh
+++ docker.sh
@@ -2,10 +2,10 @@
 
 # Build the docker image
 # -t Tags the image with the name dockerize
-docker image build -f Dockerfile -t forum-app .
+docker image build -f Dockerfile -t dockerize .
 
 # -f specifies the path
-# docker image build -f Dockerfile -t dockerize .
+
 
 # Run the docker container from the image made above
-docker container run -p 8080:8080 --name forum-app
+docker container run -p 8080:8080 --name forum dockerize
